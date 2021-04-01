7f8314a0f686fcc1b290ccb17c9f538b625158ab
ericguthrie <ericguthrie12@gmail.com>
2021-04-01 00:59:58 +0000

    sql queries

---
+++ 1_queries/most_visited_city.sql
@@ -0,0 +1,6 @@
+SELECT properties.city, count(reservations) as total_reservations
+FROM reservations
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+
